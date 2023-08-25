57dd86ad8f15c327fdfc434684e484b1aea7dbf3
Maria Joana Apolinario Ceron <133697500+MaJoApol@users.noreply.github.com>
2023-08-24 21:56:18 -0300

    Update Ex14.py

--- Lista - 1/Ex14.py
+++ Lista - 1/Ex14.py
@@ -3,15 +3,10 @@ Escreva um Programa que verifique se um elemento está na lista e verifique a po
 '''
 p = 0
 
-jogos = ["Moonlighter", "Tomb Raider", "GTA V", "Minecraft", "God of War", "Moonlighter", "GTA V"]
+jogos = ["Tomb Raider", "Minecraft", "God of War", "Moonlighter", "GTA V"]
 print(jogos)
 
 elemento = input("Que jogo você quer procurar na lista? \n")
 
 if elemento in jogos:
-    for i in jogos:
-        while i != elemento:
-            p += 1
-else:
-    print("\nO jogo não está na lista")
-print(f"\nO jogo {elemento} está na posição {p}")
+    print(f"\nO jogo {elemento} está na posição {jogos.index(elemento)}")
